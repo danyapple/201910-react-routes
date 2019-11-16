9a4f438491ee9a328c89344b075de39afe24cc65
danyapple <danyapple19@yahoo.es>
2019-11-16 07:09:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 201910-react-second-esmbcw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/201910-react-second-esmbcw)
---
+++ index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Form from './src/components/Form/Form';
+import './style.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      //Primero indicamos las rutas con la etiqueta <Router>
+      <Router>
+         <Switch>
+            <Route path = "/home" component={Home}/>
+            <Route path = "/form" component={Form}/>
+            <Route component={Home}/>
+         </Switch>
+      </Router>
+    );
+  };
+}
+
+const Home = (props) => {
+  const goToForm = () => {
+    props.history.push("/form");
+  };
+  return <button onClick={goToForm}>Go!</button>
+};
+
+render(<App />, document.getElementById('root'));
